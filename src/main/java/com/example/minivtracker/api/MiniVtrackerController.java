e6e47d0d9bff9ad7b268607ebb92b974060559ac
Gabriel <rocha@pop-os.localdomain>
2022-10-03 15:58:04 -0300

    adição de endpoint provedores e collection postman

--- src/main/java/com/example/minivtracker/api/MiniVtrackerController.java
+++ src/main/java/com/example/minivtracker/api/MiniVtrackerController.java
@@ -1,11 +1,13 @@
 package com.example.minivtracker.api;
 
 import com.example.minivtracker.dto.BaseRequestDto;
+import com.example.minivtracker.enums.Providers;
 import com.example.minivtracker.services.NewsapiService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -21,5 +23,10 @@ public class MiniVtrackerController {
     @PostMapping("/news")
     public Map<String, Object> getNews (@RequestBody BaseRequestDto request) {
         return newsapiService.getNewsFromApis(request);
+    }
+
+    @GetMapping("/providers")
+    public List<String> getProviders() {
+        return Providers.getProviders();
     }
 }
